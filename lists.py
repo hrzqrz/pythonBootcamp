00e87eb91beea7cfaacb9de34a5321b960908c61
hrzqrz <hrzqrz@gmail.com>
2020-09-19 18:02:55 +0430

    Lists in python

---
+++ lists.py
@@ -0,0 +1,18 @@
+names = ['Safa', 'Roza']
+print(names)
+
+names.insert(0, 'Marta')
+print(names)
+print(names[2])
+del(names[0])
+
+print(names)
+
+print(len(names))
+
+names[0] = "Parsa"
+
+print(names)
+
+listWithDiffrentTypes = ['Safa', 'Roza', 39, 7, 34.5]
+print(listWithDiffrentTypes)
