4e3b46b954f9ca8cc88367bbb38b2c5c21862bd5
hrzqrz <hrzqrz@gmail.com>
2020-09-19 17:07:15 +0430

    If statement and comments in python

---
+++ ifStatement.py
@@ -0,0 +1,16 @@
+age = 14
+name = "Safa"
+
+print("Hello my name is {} and i am {} years old.".format(name, age))
+
+#this is an if statement
+
+""" this
+is multi
+line
+comment
+"""
+if age > 18:
+    print("{} you are satisfied to do that.".format(name))
+else:
+    print("{} you are still too young.".format(name))
